0621815c80feb701241c22b860df91591f733c46
mavizfahim <152947606+mavizfahim@users.noreply.github.com>
2024-01-23 23:50:52 -0800

    added

---
+++ session1.js
@@ -0,0 +1,33 @@
+// var huss = "hussain abdullah"
+// if(huss=="hussain abdullah"){
+//     console.log("hussain abdullah")
+// }
+// else{
+//     console.log("shahid ismail")
+// }
+
+
+// for(let huss=1;huss<=100;huss++){
+//     console.log("hussain abdullah")
+// }
+
+// var i=10
+// while(i>=0){
+//     console.log(i)
+// }
+
+// function multiply (a,b,c){
+//     console.log(a*b*c)
+// }
+// multiply(6,9,8)
+
+// function displayname (username){
+//     console.log("welcome " + username)
+// }
+// displayname("hussain abdullah")
+// displayname("shahid ismail")
+// displayname("usama adeel")
+
+
+
+
