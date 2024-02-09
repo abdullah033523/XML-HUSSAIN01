abd163124b71ba4ec928b27d67b5f1402b6bdb3f
abdullah033523 <154964521+abdullah033523@users.noreply.github.com>
2024-02-08 22:51:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function newfunction(){
    let huss1 = document.getElementById("huss").value
    const huss2 =  document.querySelector("ul")
    huss2.innerHTML += `<li>${huss1}   <button onclick="deleteit(this)">DELETE</button>
-   <button onclick="updateit()">UPDATE</button> </li>`
+   <button onclick="updateit(this)">UPDATE</button> </li>`
    document.getElementById("huss").value=("")
 }
 
@@ -10,6 +10,11 @@ function deleteit(getobject){
     console.log(getobject)
     getobject.parentElement.remove()
 }
+function updateit(getobject){
+    getobject.parentElement.children[0].setAttribute("contentEditable",true)
+    
+}
+
 
 
 
